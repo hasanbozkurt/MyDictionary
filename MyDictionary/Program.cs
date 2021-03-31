4adad00110cbcc9373ba766af3ca177c42705fc4
Hasan Bozkurt <gsli_hasan27@hotmail.com>
2021-03-31 23:14:36 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> productList = new MyDictionary<string>();
+            productList.Add("Laptop");
+            productList.Add("Yazıcı");
+            productList.Add("Monitör");
+            productList.Add("Klavye");
+            productList.Add("Mouse");
+            productList.Add("Kamera");
+            productList.Add("Hoparlör");
+            productList.Add("Kulaklık");
+            productList.Add("Gaming Masa");
+            productList.Add("Gaming Koltuk");
+            
+
+            Console.WriteLine("Ürün Sayısı: "+productList.Length);
+
+
+            Console.WriteLine("-----------Ürün Listesi-----------");
+            foreach (var products in productList.Items)
+            {
+                Console.WriteLine(products);
+            }
+        }
+    }
+}
